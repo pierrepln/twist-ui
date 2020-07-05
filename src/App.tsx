9423b4928b244f9fbd3e835950d265d1ef40cf27
Pierre Plantié <pierre.plantie@gmail.com>
2020-07-05 09:38:38 +0100

    Add Login feature (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import LoginForm from "./LoginForm";
+import Container from "@material-ui/core/Container";
+
 
 const App = () => {
-  return <div>Hello twist-ui</div>;
+  const [user, setUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/me", {
+      credentials: "include",
+    }).then((body) =>
+      body.json().then(({email}) => {
+        setUser(email);
+      })
+    );
+  }, [user]);
+
+  /* user === null ? 
+    <div>Welcome to twist-ui</div> :  */
+  return (
+    <Container>
+      {user === null ? (
+        <div>Welcome to twist-ui</div>
+      ) : user === "user" ? (
+        <div>Logged in</div>
+      ) : (
+        <LoginForm onConnect={setUser}></LoginForm>
+      )}
+    </Container>
+  );
 };
 
 export default App;
